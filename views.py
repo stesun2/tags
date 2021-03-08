2a24c644271fdf6b9fd0f006158382423157e721
Chris Walquist <cwalquist@drw.com>
2021-03-08 16:38:50 -0600

    Add a (poorly designed) delete route

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ from tag import Tag
 @app.route('/', methods=['GET'])
 def show_tags():
     tags = Tag.select()
-    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
+    tags_html = '\n'.join(list(map(lambda x: "<a href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
     return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
@@ -16,5 +16,12 @@ def add_tag():
     Tag.get_or_create(
       name=request.form['tag-name'],
       defaults={'created_at': datetime.now(), 'updated_at': datetime.now()})
+
+    return redirect('/')
+
+# GET is not the recommended way to implement DELETE, but oh well...
+@app.route('/tags/<tag>', methods=['GET'])
+def remove_tag(tag):
+    tag_to_remove = Tag.get(Tag.name == tag).delete_instance()
 
     return redirect('/')
