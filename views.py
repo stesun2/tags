30150deaaf5f99fc5b1c5386c2d810e9ed9e6591
Chris Walquist <cwalquist@drw.com>
2021-03-08 16:16:47 -0600

    Move css into a stylesheet

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ def show_tags():
     tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
+    return "<html><head><link href=\"static/style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div></body></html>" % (app.config['config']['awesome_image'],tags_html, form_html)
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
